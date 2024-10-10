c8c0732db23941c3e069bbf6d36a9f4fc69ae043
thiago.leite <thiago.leite@aviva.com.br>
2024-10-10 14:01:25 -0300

    Implement - Validação do tipo de arquivo
    Caso não seja CSV irá mostrar erro

--- apps/tech_api/api/serializers.py
+++ apps/tech_api/api/serializers.py
@@ -7,5 +7,7 @@ class FileUploadSerializer(serializers.Serializer):
     def validate_file(self, value):
         if value.size > 50*1024*1024:
             raise serializers.ValidationError("O arquivo é muito grande. Ultrapassa o limite de 50MB.")
+        if not value.name.endswith('.csv'):
+            raise serializers.ValidationError("O arquivo deve ser um arquivo CSV.")
         
         return value
